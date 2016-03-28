a480d4ed925388cde17fe76de3d1cc3706aa2518
arthurbfcarvalho <arthurbfcarvalho@gmail.com>
2016-03-28 16:49:35 -0300

    Criação da classe Task

---
+++ Finis/Finis/Task.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Finis
+//
+//  Created by Arthur Carvalho on 3/28/16.
+//  Copyright © 2016 Alline Pedreira. All rights reserved.
+//
+
+import Foundation
+
+class Task  {
+    
+    var name: String!
+    var date: String!
+    var tag: String!
+    
+    init(name:String, date:String, tag:String) {
+        self.name = name
+        self.date = date
+        self.tag = tag
+    }    
+}
--- Finis/Finis/User.swift
+++ Finis/Finis/User.swift
@@ -10,6 +10,7 @@ import Foundation
 
 
 class User {
+    
     var name: String!
     var email: String!
     var password: String!
